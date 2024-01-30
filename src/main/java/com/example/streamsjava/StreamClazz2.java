74c6ea9111fbebf9eae548fdd52d9cd74a6909e7
Rövşən V. Məhərrəmov <rovshan.maharramov@cybernet.az>
2024-01-30 17:42:19 +0400

    Add matchers any,all,none : Streams

--- src/main/java/com/example/streamsjava/StreamClazz2.java
+++ src/main/java/com/example/streamsjava/StreamClazz2.java
@@ -8,10 +8,8 @@ import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public class StreamClazz2 {
@@ -28,12 +26,29 @@ public class StreamClazz2 {
     }
 
     public static void main(String[] args) {
+        //Terminal operators: forEach, min, max, findAny, findFirst, anyMatch..
+
         Optional<String> min = Stream.of("ABCD", "ABC", "AB", "ABCDE")
                 .min(Comparator.comparingInt(String::length));
         System.out.println("String with min length: " + min.get());
+
         Optional<Integer> max = Stream.of(3,5,1,7)
                 .max((a1, a2) -> a1 - a2);
         System.out.println("Number with max value: " + max.get());
+
+        Optional<String> res1 = Stream.of("John", "Alexa")
+                .findAny(); //John (usually);
+        res1.ifPresent(System.out::println);
+
+        Optional<String> res2 = Stream.of("Jerry", "Alex", "Den")
+                .findFirst();
+        res2.ifPresent(System.out::println);
+
+        List<String> names = Arrays.asList("Alex", "Bob", "Catalina");
+        Predicate<String> pred = name -> name.startsWith("A");
+        System.out.println("matcher1: " + names.stream().anyMatch(pred)); //true, one does
+        System.out.println("matcher2: " + names.stream().allMatch(pred)); //false, two doesn't
+        System.out.println("matcher3: " + names.stream().noneMatch(pred)); //false, one does
     }
 
     public static List<Cat> loadCats(String fileName) {
